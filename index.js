55c494eaf510c6d328cca1800f5be7a1b264f247
Andreas <iam@grml.in>
2015-09-08 15:23:19 +0200

    listeners via function

--- index.js
+++ index.js
@@ -2,40 +2,40 @@
 var cache = {};
 
 var Emitter = {
-	registerHandler(handlerName, handler, spec) {
-		if (typeof handler !== 'function') {
-			throw new Error(`Handler for the interest ${handlerName} is missing!`);
-		}
-		cache[handlerName] = cache[handlerName] || [];
-		cache[handlerName].push({
-			handler: handler,
-			spec: spec
-		});
-	},
-	dispatch(handlerName, data) {
-		if (cache[handlerName] !== undefined) {
-			window.setTimeout(()=> {
-				cache[handlerName].forEach(callbackObj => callbackObj.handler.call(callbackObj.spec, data));
-			}, 10);
-		}
-	}
+    registerHandler(handlerName, handler, spec) {
+        if (typeof handler !== 'function') {
+            throw new Error(`Handler for the interest ${handlerName} is missing!`);
+        }
+        cache[handlerName] = cache[handlerName] || [];
+        cache[handlerName].push({
+            handler: handler,
+            spec: spec
+        });
+    },
+    dispatch(handlerName, data) {
+        if (cache[handlerName] !== undefined) {
+            window.setTimeout(()=> {
+                cache[handlerName].forEach(callbackObj => callbackObj.handler.call(callbackObj.spec, data));
+            }, 10);
+        }
+    }
 };
 
 function addInterests(spec) {
-	var handlers = spec.listeners || {};
+    var listeners = typeof spec.getListeners === 'function' ? spec.getListeners() : {};
 
-	for (var handler in handlers) {
-		if (handlers.hasOwnProperty(handler)) {
-			Emitter.registerHandler(handler, spec[handlers[handler]], spec);
-		}
-	}
+    for (var handler in listeners) {
+        if (listeners.hasOwnProperty(handler)) {
+            Emitter.registerHandler(handler, spec[listeners[handler]], spec);
+        }
+    }
 }
 
 module.exports = {
-	initialize() {
-		addInterests(this);
-	},
-	emit(eventName, eventData) {
-		Emitter.dispatch(eventName, eventData);
-	}
+    initialize() {
+        addInterests(this);
+    },
+    emit(eventName, eventData) {
+        Emitter.dispatch(eventName, eventData);
+    }
 };
